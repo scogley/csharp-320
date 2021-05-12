cac36211566e455fd3affecadaa4e6f0f8eaee46
Sean Cogley <secogley@microsoft.com>
2021-05-12 09:41:27 -0700

    1.Display to the console, the names of the users where the password is "hello" and you can use a foreach here only.Hint: Where

--- Homework2/Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Homework2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Homework2
 {
@@ -7,13 +8,27 @@ namespace Homework2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             var users = new List<Models.User>();
 
             users.Add(new Models.User { Name = "Dave", Password = "hello" });
             users.Add(new Models.User { Name = "Steve", Password = "steve" });
             users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+
+            // 1.Display to the console, the names of the users where the password is "hello" and you can use a foreach here only.Hint: Where
+            var user = users.Where(p => p.Password == "hello");
+            foreach(var u in user)
+            {
+                Console.WriteLine(u.Name);
+            }
+
+            // 2.Delete any passwords that are the lower-cased version of their Name.
+            // Do not just look for "steve".It has to be data - driven.Hint: Remove or RemoveAll
+
+            // 3. Delete the first User that has the password "hello". Hint: First or FirstOrDefault
+
+            // 4. Display to the console the remaining users with their Name and Password. Hint: ForEach
+
         }
     }
 }
