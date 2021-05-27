34a85b8521836822579692f29372d8d03680b4f7
Sean Cogley <smcogley@gmail.com>
2021-05-26 18:09:01 -0700

    add DatabaseManager DriverRepository and updated the methods with my names

---
+++ final_proj/Formula1/DriverRepository/DatabaseManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DriverDB;
+
+namespace DriverRepository
+{
+    public class DatabaseManager
+    {
+        private static readonly Formula1Context entities;
+
+        // Initialize and open the database connection
+        static DatabaseManager()
+        {
+            entities = new Formula1Context();
+        }
+
+        // Provide an accessor to the database
+        public static Formula1Context Instance
+        {
+            get
+            {
+                return entities;
+            }
+        }
+    }
+}
---
+++ final_proj/Formula1/DriverRepository/DriverRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DriverDB;
+
+namespace DriverRepository
+{
+    public class ContactModel
+    {
+        public int Id { get; set; }
+        public string CircuitName { get; set; }
+        public string DriverName { get; set; }
+        public string TeamName { get; set; }
+        public string Position { get; set; }
+        public string Points { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+    }
+
+    public class ContactRepository
+    {
+        public ContactModel Add(ContactModel contactModel)
+        {
+            var driverDb = ToDbModel(contactModel);
+
+            DatabaseManager.Instance.TblDriver.Add(driverDb);
+            DatabaseManager.Instance.SaveChanges();
+
+            contactModel = new ContactModel
+            {
+                Id = driverDb.Id,
+                CircuitName = driverDb.CircuitName,
+                DriverName = driverDb.DriverName,
+                TeamName = driverDb.TeamName,
+                Position = driverDb.Position,
+                Points = driverDb.Points,
+                CreatedDate = driverDb.CreatedDate
+            };
+            return contactModel;
+        }
+
+        public List<ContactModel> GetAll()
+        {
+            // Use .Select() to map the database contacts to ContactModel
+            var items = DatabaseManager.Instance.TblDriver
+              .Select(t => new ContactModel
+              {
+                  Id = t.Id,
+                  CircuitName = t.CircuitName,
+                  DriverName = t.DriverName,
+                  TeamName = t.TeamName,
+                  Position = t.Position,
+                  Points = t.Points,
+                  CreatedDate = t.CreatedDate,
+              }).ToList();
+
+            return items;
+        }
+
+        public bool Update(ContactModel contactModel)
+        {
+            var original = DatabaseManager.Instance.TblDriver.Find(contactModel.Id);
+
+            if (original != null)
+            {
+                DatabaseManager.Instance.Entry(original).CurrentValues.SetValues(ToDbModel(contactModel));
+                DatabaseManager.Instance.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Remove(int contactId)
+        {
+            var items = DatabaseManager.Instance.TblDriver
+                                .Where(t => t.Id == contactId);
+
+            if (items.Count() == 0)
+            {
+                return false;
+            }
+
+            DatabaseManager.Instance.TblDriver.Remove(items.First());
+            DatabaseManager.Instance.SaveChanges();
+
+            return true;
+        }
+
+        private TblDriver ToDbModel(ContactModel contactModel)
+        {
+            var contactDb = new TblDriver
+            {
+                Id = contactModel.Id,
+                CircuitName = contactModel.CircuitName,
+                DriverName = contactModel.DriverName,
+                TeamName = contactModel.TeamName,
+                Position = contactModel.Position,
+                Points = contactModel.Points,
+                CreatedDate = contactModel.CreatedDate,
+            };
+
+            return contactDb;
+        }
+    }
+}
