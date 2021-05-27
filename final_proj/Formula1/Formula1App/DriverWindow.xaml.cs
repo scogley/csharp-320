5da58e0a403546ef83e0364b70d719411023e76c
Sean Cogley <smcogley@gmail.com>
2021-05-26 19:18:13 -0700

    step 9 complete http://cstructor.com/Home/SNotes?classId=49&sequence=520

--- final_proj/Formula1/Formula1App/DriverWindow.xaml.cs
+++ final_proj/Formula1/Formula1App/DriverWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Formula1App.Models;
 
 namespace Formula1App
 {
@@ -20,6 +21,32 @@ namespace Formula1App
         public DriverWindow()
         {
             InitializeComponent();
+            // Don't show this window in the task bar
+            ShowInTaskbar = false;
+        }
+
+        public DriverModel Driver { get; set; }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Driver = new DriverModel();
+
+            Driver.DriverName = uxDriverName.Text;
+            Driver.TeamName = uxTeamName.Text;
+            Driver.Position = uxPosition.Text;
+            
+            Driver.CreatedDate = DateTime.Now;
+
+            // This is the return value of ShowDialog( ) below
+            DialogResult = true;
+            Close();
+        }
+
+        private void uxCancel_Click(object sender, RoutedEventArgs e)
+        {
+            // This is the return value of ShowDialog( ) below
+            DialogResult = false;
+            Close();
         }
     }
 }
