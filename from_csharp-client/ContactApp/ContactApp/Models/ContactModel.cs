9ee4815c1057c5bb2d29b5df9f02c3b3eb4b4289
Sean Cogley <smcogley@gmail.com>
2021-05-27 20:57:27 -0700

    data validation for name is working

--- from_csharp-client/ContactApp/ContactApp/Models/ContactModel.cs
+++ from_csharp-client/ContactApp/ContactApp/Models/ContactModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
+
 
 namespace ContactApp.Models
 {
-    public class ContactModel
+    public class ContactModel : IDataErrorInfo, INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -17,6 +19,62 @@ namespace ContactApp.Models
         internal ContactModel Clone()
         {
             return (ContactModel)this.MemberwiseClone();
+        }
+
+        private string nameError { get; set; }
+
+        // INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // IDataErrorInfo interface
+        public string Error => "Never Used";
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case "Name":
+                        {
+                            NameError = "";
+
+                            if (Name == null || string.IsNullOrEmpty(Name))
+                            {
+                                NameError = "Name cannot be empty.";
+                            }
+                            else if (Name.Length > 12)
+                            {
+                                NameError = "Name cannot be longer than 12 characters.";
+                            }
+
+                            return NameError;
+                        }
+                }
+
+                return null;
+            }
+        }
+
+        public string NameError
+        {
+            get
+            {
+                return nameError;
+            }
+            set
+            {
+                if (nameError != value)
+                {
+                    nameError = value;
+                    OnPropertyChanged("NameError");
+                }
+            }
         }
 
         public ContactRepository.ContactModel ToRepositoryModel()
