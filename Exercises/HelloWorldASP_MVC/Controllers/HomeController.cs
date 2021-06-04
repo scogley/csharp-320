6e99484a2eefe6c9ac8f04fc8c1eecbf0a73feab
Sean Cogley <smcogley@gmail.com>
2021-06-03 20:33:24 -0700

    working hello world asp.net about to start exercise to add email

---
+++ Exercises/HelloWorldASP_MVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorldASP_MVC.Models;
+
+
+namespace HelloWorldASP_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View(new GuestResponse());
+        }
+        [HttpPost]
+        public IActionResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            return View("Thanks", guestResponse);
+        }
+    }
+}
---
+++ Exercises/HelloWorldASP_MVC/Models/GuestResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldASP_MVC.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
