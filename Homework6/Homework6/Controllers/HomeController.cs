0ca657fde5b2c116c3721e5de4c7582e317bc3fb
Sean Cogley <smcogley@gmail.com>
2021-06-16 17:54:03 -0700

    homework 6 in progress

---
+++ Homework6/Homework6/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Homework6.Models;
+
+namespace Homework6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Receive the data coming from the browser
+        [HttpPost]
+        public IActionResult SendCardForm(Models.Response cardSentResponse)
+        {
+            return View(new Response());
+        }
+    }
+}
---
+++ Homework6/Homework6/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework6.Models
+{
+    public class Response
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Message { get; set; }
+    }
+}
